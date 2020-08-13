99f5d111452fa752907446a58b914226f6186fd9
verbalk <verbalk89@naver.com>
2020-08-13 20:12:04 +0900

    0813

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
+import { Pane, Button, Heading, Text, Select } from 'evergreen-ui';
+import { MovieItem } from './MovieItem';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [genres] = useState(['장르별', '스릴러', '공포', '드라마', '코미디']);
+  const [selectedGenres, setSelectedGenres] = useState('장르별');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Pane clearfix>
+      <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
+        <Pane flex={1} alignItems="center" display="flex">
+          <Heading size={600}>영화 추천 리스트</Heading>
+        </Pane>
+        <Pane>
+          <Select
+            value={selectedGenres}
+            onChange={(event) => setSelectedGenres(event.target.value)}
+          >
+            {genres.map((genreItem) => (
+              <option key={genreItem} value={genreItem}>
+                {genreItem}
+              </option>
+            ))}
+          </Select>
+        </Pane>
+      </Pane>
+      {movies.map((movieItem) => {
+        return <MovieItem title="awef" contents="awef" src="awjefio" />;
+      })}
+
+      <MovieItem title="다만악에서구하소서" contents="공포" src="hello.png" />
+      <MovieItem title="다만악에서구하소서" contents="공포" src="hello.png" />
+      <MovieItem title="다만악에서구하소서" contents="공포" src="hello.png" />
+    </Pane>
   );
 }
 
---
+++ src/MovieItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Pane, Text } from 'evergreen-ui';
+
+export const MovieItem = ({ title, contents, src }) => {
+  return (
+    <Pane
+      elevation={3}
+      float="left"
+      width={200}
+      height={120}
+      margin={24}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <img src={src} alt="영화썸네일"></img>
+      <Text>{title}</Text>
+      <Text size={300}>{contents}</Text>
+    </Pane>
+  );
+};
